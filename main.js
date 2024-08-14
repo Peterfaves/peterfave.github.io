84df2ae2ef1a95b31c9e84caea93dbda593a58b6
Peterfaves <125551588+Peterfaves@users.noreply.github.com>
2024-08-14 01:20:51 +0100

    Add files via upload
    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+  strings:["Frontend Developer" , "Wordpress Developer" , "Web Developer" , "Web designer"],
+  typespeed: 100,
+  backspeed: 100,
+  backDelay: 1000,
+  loop:true
+});
